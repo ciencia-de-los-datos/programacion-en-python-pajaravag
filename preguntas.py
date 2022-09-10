a0890b8ba4a032f8bab5e94ca14cce8cc7d7a36b
Pablo Andres <codechemfin@gmail.com>
2022-09-10 13:51:01 -0500

    Update preguntas.py

--- preguntas.py
+++ preguntas.py
@@ -13,7 +13,6 @@ Utilice el archivo `data.csv` para resolver las preguntas.
 """
 with open('data.csv', 'r') as file:
     data = file.readlines()
-    print(data)
 data = [line.replace('\n', '') for line in data]
 data = [line.replace('\t', ',') for line in data]
 data = [line.split(',') for line in data]
@@ -30,6 +29,7 @@ def pregunta_01(data):
     ## Aqui va mi codigo
     result = 0.0
     col_2 = [pos[1] for pos in data]
+    print(col_2)
     for i in col_2:
         result += i 
     return result
